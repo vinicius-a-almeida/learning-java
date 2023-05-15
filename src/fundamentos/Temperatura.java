bedf396acd4e16232c06c55cbc54543bf84add60
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2023-05-15 12:16:02 -0300

    feat: converting F to C

---
+++ src/fundamentos/Temperatura.java
@@ -0,0 +1,16 @@
+package fundamentos;
+import java.util.Scanner;
+
+// atividade convertendo fahrenheit para celsius
+
+public class Temperatura {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final int i = 32;
+        final double j = 5 / 9.0;
+        System.out.println("Digite a temperatura em fahrenheit: ");
+        int f = input.nextInt();
+        System.out.println("O resultado da conversão é: " + (f - i) * j);
+        input.close();
+    }
+}
