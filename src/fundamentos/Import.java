29f7e9eeba399c10a74ac9ac91575a70db8e3634
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2023-05-18 13:53:47 -0300

    feat: imports

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+import java.util.Date;
+
+
+// imports ficam fora da classe assim como os pacotes
+
+public class Import {
+    public static void main(String[] args) {
+        java.lang.String b = "Boa tarde!";
+        System.out.println(b);
+
+        String s = "Bom dia!";
+        System.out.println(s);
+        Date d = new Date();
+        System.out.println(d);
+
+        //JButton botao = new JButton();
+    }
+}
